e254674612118c4ad5ebbcc4ebee4e8fc0ffc870
gndhstr <ghandhisatria@gmail.com>
2023-07-23 13:47:11 +0700

    adding navigation

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './navigation.css'
 
+import esc from '../../assets/esc_white.png'
+
+import {Link} from 'react-scroll'
+
 const navigation = () => {
   return (
-    <div>navigation</div>
+    <nav className='navi'>
+      <div className='navi__img'>
+      <img src={esc} alt="" />
+      </div>
+      <div className='navi__txt'>
+          <Link activeClass='active' spy to="home" >Home</Link>
+          <Link activeClass='active' spy to="kegiatan" >Kegiatan</Link>
+          <Link activeClass='active' spy to="keanggotaan">Keanggotaan</Link>
+          <Link activeClass='active' spy to="prestasi" >Prestasi</Link>
+          <Link activeClass='active' spy to="contact" >Contact</Link>
+      </div>
+    </nav>
   )
 }
 
