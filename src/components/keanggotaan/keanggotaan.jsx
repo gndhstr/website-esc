f019c3578d088dbda0fda976d6fb25cef6ee0cfb
gndhstr <ghandhisatria@gmail.com>
2023-07-25 22:12:26 +0700

    card section update

--- src/components/keanggotaan/keanggotaan.jsx
+++ src/components/keanggotaan/keanggotaan.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { useState } from 'react';
 import './keanggotaan.css'
 
 import heroimg from '../../assets/heroimg-contact.png'
 
-const keanggotaan = () => {
+import Gopreto from '../../assets/profile/gop-software.png'
+
+function Keanggotaan () {
+
+  const [cards] = useState([
+    {
+      img : 'https://drive.google.com/uc?export=view&id=19jmsgM_yXQvbzi6kEpVq6hSW8elGx0tS',
+      text:'PSDM',
+    },
+    {
+      img : 'https://drive.google.com/uc?export=view&id=1JSvKInXb5HtV-XyCU2c-cd9egho77Lvc',
+      text:'Pengurus Harian',
+    },
+    {
+      img : 'https://drive.google.com/uc?export=view&id=14rXaEBoGmFLFC4ZFyQuOC3_wLNEUkiOK',
+      text:'BRT',
+    },
+    {
+      img : 'https://drive.google.com/uc?export=view&id=11LbdDwn8E4WyAX1ORSf0RqVTcKRlIINX',
+      text:'Software',
+    },
+    {
+      img : 'https://drive.google.com/uc?export=view&id=1VIw-dp3YURWdgPQtTJzdWsn7DqQFk7mW',
+      text:'Komp-Info',
+    },
+    {
+      img : 'https://drive.google.com/uc?export=view&id=1nBHiJ0aXyCzmVd02y2KuSN3SdClQzsgB',
+      text:'Hardware',
+    },
+  ])
+
   return (
     <div className='container container__anggota'>
       <div className='hero__anggota'>
@@ -12,14 +43,28 @@ const keanggotaan = () => {
           <p>Electrical Science Club memiliki beberapa divisi seperti Hardware, Software, Kominfo, PH, BRT, dan PSDM.</p>
         </div>
         <div className='heroa__right'>
-          <img src={heroimg} alt="contact-icon" />
+          <img src={heroimg} alt="anggota-icon" />
         </div>
       </div>
-      <div>
+      <div className='cardsection__anggota'>
+        { cards.map((card, i) => (
 
+          <a href="#" key={i} className='card__anggota'>
+          <div className='card__header'>
+            {/* left this empty! */}
+          </div>
+          <div className='card__profile'>
+            <img src={card.img} alt="profile-img" className='img__profile'/>
+          </div>
+          <div className='card__text'>
+            <h3>{card.text}</h3>
+          </div>
+          </a>
+
+        ))}
       </div>
     </div>
   )
 }
 
-export default keanggotaan
+export default Keanggotaan
