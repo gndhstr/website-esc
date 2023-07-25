d0a4200a2ad6337c9a249f0caf393b8b54603107
gndhstr <ghandhisatria@gmail.com>
2023-07-25 19:32:40 +0700

    adding contact hero-section

--- src/components/keanggotaan/keanggotaan.jsx
+++ src/components/keanggotaan/keanggotaan.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './keanggotaan.css'
 
+import heroimg from '../../assets/heroimg-contact.png'
+
 const keanggotaan = () => {
   return (
-    <div>
-      <div className=''>
+    <div className='container container__anggota'>
+      <div className='hero__anggota'>
+        <div className='heroa__left'>
+          <h1>Keanggotaan Electrical Science Club</h1>
+          <p>Electrical Science Club memiliki beberapa divisi seperti Hardware, Software, Kominfo, PH, BRT, dan PSDM.</p>
+        </div>
+        <div className='heroa__right'>
+          <img src={heroimg} alt="contact-icon" />
+        </div>
       </div>
       <div>
 
