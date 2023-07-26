50d1fc41f441e2e888d216c15df3d6dbffc17d6e
gndhstr <ghandhisatria@gmail.com>
2023-07-26 12:45:22 +0700

    prestasi hero

--- src/components/prestasi/prestasi.jsx
+++ src/components/prestasi/prestasi.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './prestasi.css'
 
+import heroimg from '../../assets/heroimg-prestasi.png'
+
 const prestasi = () => {
   return (
-    <div>prestasi</div>
+    <div className='container prestasi__container'>
+      <div className='hero__anggota'>
+        <div className='heroa__left'>
+          <h1>Prestasi Electrical Science Club</h1>
+          <p>Electrical Science Club memfasilitasi para mahasiswa Teknik Elektro untuk bergerak dalam bidang riset dan keilmiahan.</p>
+        </div>
+        <div className='heroa__right'>
+          <img src={heroimg} alt="anggota-icon" />
+        </div>
+      </div>
+    </div>
   )
 }
 
