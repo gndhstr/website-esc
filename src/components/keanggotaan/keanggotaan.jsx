918e2b8a3a96d6a5bc563c21d52596b44b55fded
gndhstr <ghandhisatria@gmail.com>
2023-07-26 12:44:18 +0700

    keanggotaan page

--- src/components/keanggotaan/keanggotaan.jsx
+++ src/components/keanggotaan/keanggotaan.jsx
@@ -4,32 +4,36 @@ import './keanggotaan.css'
 
 import heroimg from '../../assets/heroimg-contact.png'
 
-import Gopreto from '../../assets/profile/gop-software.png'
-
 function Keanggotaan () {
 
   const [cards] = useState([
     {
+      link : '/Keanggotaan/PSDM',
       img : 'https://drive.google.com/uc?export=view&id=19jmsgM_yXQvbzi6kEpVq6hSW8elGx0tS',
       text:'PSDM',
     },
     {
+      link : '/Keanggotaan/PH',
       img : 'https://drive.google.com/uc?export=view&id=1JSvKInXb5HtV-XyCU2c-cd9egho77Lvc',
       text:'Pengurus Harian',
     },
     {
+      link : '/Keanggotaan/BRT',
       img : 'https://drive.google.com/uc?export=view&id=14rXaEBoGmFLFC4ZFyQuOC3_wLNEUkiOK',
       text:'BRT',
     },
     {
+      link : '/Keanggotaan/Software',
       img : 'https://drive.google.com/uc?export=view&id=11LbdDwn8E4WyAX1ORSf0RqVTcKRlIINX',
       text:'Software',
     },
     {
+      link : '/Keanggotaan/Komp-Info',
       img : 'https://drive.google.com/uc?export=view&id=1VIw-dp3YURWdgPQtTJzdWsn7DqQFk7mW',
       text:'Komp-Info',
     },
     {
+      link : '/Keanggotaan/Hardware',
       img : 'https://drive.google.com/uc?export=view&id=1nBHiJ0aXyCzmVd02y2KuSN3SdClQzsgB',
       text:'Hardware',
     },
@@ -49,7 +53,7 @@ function Keanggotaan () {
       <div className='cardsection__anggota'>
         { cards.map((card, i) => (
 
-          <a href="#" key={i} className='card__anggota'>
+          <a href={card.link} key={i} className='card__anggota'>
           <div className='card__header'>
             {/* left this empty! */}
           </div>
